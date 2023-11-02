adfc9eba64a1ecf0be9d21fc931610fc3f676c13
jasonkencana1 <jasonalfonza.kencana@edu.sait.ca>
2023-11-02 10:34:12 -0600

    lab 3 finished

--- App.jsx
+++ App.jsx
@@ -4,16 +4,23 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {SafeAreaView} from 'react-native';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 
 
 function App() {
+  const [tasks] = useState([
+    'Do laundry',
+    'Go to the gym',
+    'Walk the dog'
+  ]);
+
+
   return (
     <SafeAreaView>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
